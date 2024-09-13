a204f4ef3406201b290b76a24650577aeacb9b68
David Greenland <David.Greenland@brighthr.com>
2024-09-13 13:42:29 +0100

    YLW-3110: Printed values to the console.

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -4,3 +4,8 @@ var year = 2024;
 var time = 0.0;
 var name = "Abdullah";
 var isWarm = false;
+
+Console.WriteLine($"This is an integer value: {year}");
+Console.WriteLine($"This is a double value: {time}");
+Console.WriteLine($"This is a string value: {name}");
+Console.WriteLine($"This is a boolean value: {isWarm}");
