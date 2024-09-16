373c79f96c10c23a3edc34e4927e967c88d077a7
David Greenland <David.Greenland@brighthr.com>
2024-09-16 10:53:57 +0100

    Implements a for loop that prints 1 to 10.

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -10,3 +10,8 @@ Console.WriteLine($"This is an integer value: {year}");
 Console.WriteLine($"This is a double value: {timeToString}");
 Console.WriteLine($"This is a string value: {name}");
 Console.WriteLine($"This is a boolean value: {isWarm}");
+
+for (var i = 1; i <= 10; ++i)
+{
+    Console.WriteLine(i);
+}
