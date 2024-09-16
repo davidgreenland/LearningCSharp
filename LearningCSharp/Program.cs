e83a7b12a85f125bbace0c5a282754b07fd678ec
David Greenland <David.Greenland@brighthr.com>
2024-09-16 11:21:46 +0100

    Formatting text.

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -12,14 +12,14 @@ Console.WriteLine($"This is a string value: {name}");
 Console.WriteLine($"This is a boolean value: {isWarm}");
 Console.WriteLine();
 
-Console.WriteLine("The following numbers are printed with a for loop");
+Console.WriteLine("The following numbers are printed with a for loop:");
 for (var i = 1; i <= 10; ++i)
 {
     Console.WriteLine(i);
 }
 Console.WriteLine();
 
-Console.WriteLine("The following numbers are printed with a while loop");
+Console.WriteLine("The following numbers are printed with a while loop:");
 var j = 1;
 while (j <= 10)
 {
@@ -29,7 +29,7 @@ while (j <= 10)
 Console.WriteLine();
 Console.WriteLine();
 
-Console.WriteLine("The following numbers are printed with a do-while loop");
+Console.WriteLine("The following numbers are printed with a do-while loop:");
 
 var k = 1;
 do
@@ -40,14 +40,15 @@ do
 Console.WriteLine();
 Console.WriteLine();
 
-Console.WriteLine("A random number between 1 and 10 is generated. An if-else statement checks if a number is even or odd, and outputs the result:");
+Console.WriteLine("A random number between 1 and 10 is generated.");
+Console.WriteLine("An if-else statement checks if a number is even or odd, and outputs the result:");
 Random rnd = new Random();
 int number = rnd.Next(1, 11);
 if (number % 2 == 0)
 {
-    Console.WriteLine($"{number} is an even number");
+    Console.WriteLine($"{number} is an even number.");
 }
 else
 {
-    Console.WriteLine($"{number} is an odd number");
+    Console.WriteLine($"{number} is an odd number.");
 }
