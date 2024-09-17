6e50f7401d610fafa1e8bf2194db8e2559fa6dc6
David Greenland <David.Greenland@brighthr.com>
2024-09-17 13:44:47 +0100

    YLW-3113: Created Person class with Name and Age properties. Added Greet method.

---
+++ BasicsOfOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿var person = new Person("Philippa", 30);
+person.Greet();
+
+public class Person
+{
+    public string Name;
+    public int Age;
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine($"Hello {Name}.");
+    }
+}
