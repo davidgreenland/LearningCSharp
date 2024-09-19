8783a936470ee91085e1122c7906e80ebf5a0320
David Greenland <David.Greenland@brighthr.com>
2024-09-19 15:07:12 +0100

    YLW-3113: Made Utils class for CheckIfEven

--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -17,18 +17,6 @@ LoopDemonstration.ForLoop(5);
 LoopDemonstration.WhileLoop(1, 19, 2);
 LoopDemonstration.DoWhileLoop(1, 10);
 LoopDemonstration.DoWhileLoop(0, 30, 3);
-CheckIfEven(numberSet);
+Utils.CheckIfEven(numberSet);
 
 return;
-
-void CheckIfEven(int[] numbers)
-{
-    Console.WriteLine("For each number in an array, a method checks if the number is even or odd and prints the result:");
-    foreach (var number in numbers)
-    {
-        var isEven = IsEven(number);
-        Console.WriteLine($"{number} is an {(isEven ? "even" : "odd")} number.");
-    }
-}
-
-bool IsEven(int number) => number % 2 == 0;
