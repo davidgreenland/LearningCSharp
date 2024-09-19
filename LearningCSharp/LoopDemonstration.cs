107b56a5ab96a060ef950cb054c9b3141ad6907c
David Greenland <David.Greenland@brighthr.com>
2024-09-19 14:29:55 +0100

    YLW-3113: Made LoopDemonstration class with static members.

---
+++ LearningCSharp/LoopDemonstration.cs
@@ -0,0 +1,40 @@
+﻿namespace LearningCSharp;
+
+public class LoopDemonstration
+{
+    public static void ForLoop(int length)
+    {
+        Console.WriteLine("The following numbers are printed with a for loop:");
+        for (var i = 1; i <= length; ++i)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine();
+    }
+
+    public static void WhileLoop(int start, int end, int increment)
+    {
+        Console.WriteLine($"Using a while loop, a list of all numbers starting at {start}, incrementing by {increment}, ending at {end}:");
+        var i = start;
+        while (i <= end)
+        {
+            Console.Write($"{i} ");
+            i += increment;
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+
+    public static void DoWhileLoop(int start, int end, int increment = 1)
+    {
+        Console.WriteLine($"The following numbers are printed with a do-while loop{(increment != 1 ? $", incrementing by {increment}" : "")}:");
+        var i = start;
+        do
+        {
+            Console.Write($"{i} ");
+            i += increment;
+        } while (i <= end);
+        Console.WriteLine();
+        Console.WriteLine();
+    }
+}
--- LearningCSharp/Program.cs
+++ LearningCSharp/Program.cs
@@ -13,47 +13,13 @@ var numberSet = new int[] { 2, 5, 7, 9, 13, 3 };
 
 person.Greet();
 nativeVariables.PrintDefinitions();
-ForLoopDemonstration(5);
-WhileLoopDemonstration(1, 19, 2);
-DoWhileLoopDemonstration(1, 10);
-DoWhileLoopDemonstration(0, 30, 3);
+LoopDemonstration.ForLoop(5);
+LoopDemonstration.WhileLoop(1, 19, 2);
+LoopDemonstration.DoWhileLoop(1, 10);
+LoopDemonstration.DoWhileLoop(0, 30, 3);
 CheckIfEven(numberSet);
 
-void ForLoopDemonstration(int length)
-{
-    Console.WriteLine("The following numbers are printed with a for loop:");
-    for (var i = 1; i <= length; ++i)
-    {
-        Console.WriteLine(i);
-    }
-    Console.WriteLine();
-}
-
-void WhileLoopDemonstration(int start, int end, int increment)
-{
-    Console.WriteLine($"Using a while loop, a list of all numbers starting at {start}, incrementing by {increment}, ending at {end}:");
-    var i = start;
-    while (i <= end)
-    {
-        Console.Write($"{i} ");
-        i += increment;
-    }
-    Console.WriteLine();
-    Console.WriteLine();
-}
-
-void DoWhileLoopDemonstration(int start, int end, int increment = 1)
-{
-    Console.WriteLine($"The following numbers are printed with a do-while loop{(increment != 1 ? $", incrementing by {increment}" : "")}:");
-    var i = start;
-    do
-    {
-        Console.Write($"{i} ");
-        i += increment;
-    } while (i <= end);
-    Console.WriteLine();
-    Console.WriteLine();
-}
+return;
 
 void CheckIfEven(int[] numbers)
 {
