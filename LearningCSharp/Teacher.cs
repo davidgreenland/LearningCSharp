78f91db9600888b6dbb827838e25df4b456f23f8
David Greenland <David.Greenland@brighthr.com>
2024-09-24 09:23:05 +0100

    YLW-3114: Added Teacher class, extends Person with Department property.

---
+++ LearningCSharp/Teacher.cs
@@ -0,0 +1,11 @@
+﻿using LearningCSharp;
+
+class Teacher: Person
+{
+    public string Department { get; protected set; }
+
+    public Teacher(string name, int age, string department) : base(name, age) 
+    {
+        Department = department;
+    }
+}
