50bbe2fee42dc36388e8a59bee575c9fc5aa53e9
David Greenland <David.Greenland@brighthr.com>
2024-09-30 13:41:34 +0100

    YLW-3116b: created object extension to provide getDefinition method. Created IDefinitionPrinter interface.

---
+++ LearningCSharp/NativeVariables/Extensions/ObjectExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningCSharp.NativeVariables.Extensions;
+
+public static class ObjectExtensions
+{
+    public static string GetDefinition(this object obj)
+    {
+        return $"This is a {obj.GetType()} value: {obj}";
+    }
+}
---
+++ LearningCSharp/NativeVariables/NativeVariables.cs
@@ -0,0 +1,18 @@
+﻿namespace LearningCSharp.NativeVariables;
+
+public class NativeVariables
+{
+    public IEnumerable<object> Variables { get; set; }
+    private readonly IDefinitionPrinter _printer;
+
+    public NativeVariables(IEnumerable<object> variables, IDefinitionPrinter printer)
+    {
+        Variables = variables;
+        _printer = printer;
+    }
+
+    public void PrintDefinitions()
+    {
+        _printer.Print(Variables);
+    }
+}
